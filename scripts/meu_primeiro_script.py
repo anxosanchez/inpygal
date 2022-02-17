059233a076cd03bdf7335138d5d2b08058658c0a
Anxo Sánchez <asanchez@uvigo.es>
2022-02-17 19:08:19 +0100

    commit

---
+++ scripts/meu_primeiro_script.py
@@ -0,0 +1,11 @@
+import math
+
+print("Olá xente do curso de simulación")
+print("¿Cántos hai hoxe en clase?")
+
+number = input()
+number = int(number)
+root = math.sqrt(number)
+
+print("Espero que aprendades moito")
+print("Por certo, a raiz de %i é %f" %(number, root))
